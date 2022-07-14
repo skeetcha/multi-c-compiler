1092472a4100755452b2c6e7e5a80b82deace323
Cass Unterholzner <cityboundforest@gmail.com>
2022-07-13 23:22:29 -0700

    Adding C code from first tutorial (code is quite similar to tutorial as tutorial was written in C but I varied on it a bit by using my previous iterations as inspiration as well as my usual coding paradigm that I use for programming in C)

---
+++ c/token.h
@@ -0,0 +1,11 @@
+#ifndef TOKEN_H
+#define TOKEN_H
+
+#include "tokens.h"
+
+typedef struct {
+    Tokens token;
+    int intValue;
+} Token;
+
+#endif
---
+++ c/tokens.h
@@ -0,0 +1,13 @@
+#ifndef TOKENS_H
+#define TOKENS_H
+
+typedef enum {
+    T_EOF,
+    T_PLUS,
+    T_MINUS,
+    T_STAR,
+    T_SLASH,
+    T_INTLIT
+} Tokens;
+
+#endif
