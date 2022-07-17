8dd960f776ee463ffcd3cdc53652930932cd6586
Cass Unterholzner <cityboundforest@gmail.com>
2022-07-17 14:03:38 -0700

    Rust code is now up to speed with Python

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,4 +1,7 @@
-use std::{env::args, process::exit, fs::{File, write}, io::Read, str::FromStr, path::Path};
+extern crate inkwell;
+
+use std::{env::args, process::exit, fs::{File}, io::Read, sync::Arc, path::Path};
+use inkwell::{module::{Module, Linkage}, context::Context, values::{BasicValueEnum, BasicValue, FunctionValue}, builder::Builder, targets::{InitializationConfig, Target, FileType, TargetMachine, RelocMode, CodeModel}, OptimizationLevel, AddressSpace};
 
 #[derive(Clone)]
 enum Token {
@@ -76,10 +79,11 @@ struct Compiler {
     index: usize,
     line: i64,
     putback: char,
-    token: Token
+    token: Token,
+    module_ctx: Context
 }
 
-impl Compiler {
+impl<'a> Compiler {
     fn next(&mut self) -> char {
         let mut c: char = '\0';
 
@@ -183,7 +187,8 @@ impl Compiler {
             putback: '\n',
             in_data: buffer.to_owned(),
             index: 0,
-            token: Token::Eof
+            token: Token::Eof,
+            module_ctx: Context::create()
         }
     }
 
@@ -293,6 +298,72 @@ impl Compiler {
         }
     }
 
+    fn build_ast(&'a self, node: ASTNode, builder: &'a Builder) -> BasicValueEnum<'a> {
+        let mut left_val: Option<BasicValueEnum> = None;
+        let mut right_val: Option<BasicValueEnum> = None;
+
+        if let Some(left_node) = node.left.clone() {
+            left_val = Some(self.build_ast(*left_node, builder));
+        }
+
+        if let Some(right_node) = node.right.clone() {
+            right_val = Some(self.build_ast(*right_node, builder));
+        }
+
+        match node.op.clone() {
+            ASTNodeOp::Add => builder.build_int_add(left_val.unwrap().into_int_value(), right_val.unwrap().into_int_value(), "add").as_basic_value_enum(),
+            ASTNodeOp::Subtract =>  builder.build_int_sub(left_val.unwrap().into_int_value(), right_val.unwrap().into_int_value(), "sub").as_basic_value_enum(),
+            ASTNodeOp::Multiply => builder.build_int_mul(left_val.unwrap().into_int_value(), right_val.unwrap().into_int_value(), "mul").as_basic_value_enum(),
+            ASTNodeOp::Divide => builder.build_int_signed_div(left_val.unwrap().into_int_value(), right_val.unwrap().into_int_value(), "div").as_basic_value_enum(),
+            ASTNodeOp::IntLit(val) => {
+                let val_ptr = builder.build_alloca(self.module_ctx.i32_type(), "int");
+                builder.build_store(val_ptr, self.module_ctx.i32_type().const_int(val as u64, false));
+                builder.build_load(val_ptr, "int_val")
+            }
+        }
+    }
+
+    fn gen_print(&'a self, builder: &'a Builder, val: BasicValueEnum<'a>, print_fn: FunctionValue<'a>) {
+        let const_string = self.module_ctx.const_string(b"%d\n\0", true);
+        let fmt_ptr = builder.build_alloca(const_string.get_type(), "fmt");
+        builder.build_store(fmt_ptr, const_string);
+        let fmt_string_ptr = builder.build_bitcast(fmt_ptr, self.module_ctx.i8_type().ptr_type(AddressSpace::Generic), "fmt_ptr");
+        builder.build_call(print_fn, &[fmt_string_ptr.into(), val.into()], "print");
+    }
+
+    fn generate_code(&self, node: ASTNode, out_file: &Path) {
+        let module = self.module_ctx.create_module("main_module");
+        let fn_type = self.module_ctx.i32_type().fn_type(&[self.module_ctx.i32_type().into()], false);
+        let main_fn = module.add_function("main", fn_type, None);
+        let const_string = self.module_ctx.const_string(b"\0", true);
+        let printf_fn_type = self.module_ctx.i32_type().fn_type(&[self.module_ctx.i8_type().ptr_type(AddressSpace::Generic).into()], true);
+        let printf_fn = module.add_function("printf", printf_fn_type, Some(Linkage::External));
+        let builder = self.module_ctx.create_builder();
+        let entry = self.module_ctx.append_basic_block(main_fn, "entry");
+        builder.position_at_end(entry);
+        let ret_value = self.build_ast(node, &builder);
+        self.gen_print(&builder, ret_value, printf_fn);
+        let good_return = self.module_ctx.i32_type().const_int(0, false);
+        builder.build_return(Some(&good_return));
+        module.set_triple(&TargetMachine::get_default_triple());
+        let res = module.verify();
+
+        if let Err(errors) = res {
+            panic!("{}", errors);
+        }
+        
+        Target::initialize_native(&InitializationConfig::default()).expect("Failed to initialize native target");
+        let cpu_name = TargetMachine::get_host_cpu_name();
+        let cpu_features = TargetMachine::get_host_cpu_features();
+        let target = Target::from_triple(&module.get_triple()).unwrap();
+        let machine = target.create_target_machine(&module.get_triple(), &cpu_name.to_string(), &cpu_features.to_string(), OptimizationLevel::None, RelocMode::Default, CodeModel::Default).unwrap();
+        let ass_res = machine.write_to_file(&module, FileType::Object, out_file);
+
+        if let Err(errors) = ass_res {
+            panic!("{}", errors);
+        }
+    }
+
     fn run(&mut self) -> ASTNode {
         self.scan();
         self.expr()
@@ -311,6 +382,9 @@ fn main() {
         usage(args[0].clone())
     }
 
-    let mut compiler = Compiler::new(&args[1]);
+    let mut compiler = Compiler::new(&args[1].clone());
     let node: ASTNode = compiler.run();
+    let mut out_file = Path::new(".").join(Path::new(&args[1]).file_name().unwrap());
+    out_file.set_extension("o");
+    compiler.generate_code(node, out_file.as_path());
 }
