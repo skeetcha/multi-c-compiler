acf100692ddf39c8dcc6ab454333c45534869399
Cass Unterholzner <cityboundforest@gmail.com>
2022-07-21 22:15:27 -0700

    Rust code is brought up to speed with Python

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -10,61 +10,32 @@ enum Token {
     Minus,
     Star,
     Slash,
-    IntLit(i64)
+    IntLit(i64),
+    Semi,
+    Print
 }
 
-#[derive(Clone)]
-enum ASTNodeOp {
-    Add,
-    Subtract,
-    Multiply,
-    Divide,
-    IntLit(i64)
-}
-
-impl std::fmt::Display for ASTNodeOp {
+impl std::fmt::Display for Token {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match self {
-            Self::Add => write!(f, "+"),
-            Self::Subtract => write!(f, "-"),
-            Self::Multiply => write!(f, "*"),
-            Self::Divide => write!(f, "/"),
-            Self::IntLit(val) => write!(f, "int {}", val)
+            Token::Eof => write!(f, "EOF"),
+            Token::Plus => write!(f, "+"),
+            Token::Minus => write!(f, "-"),
+            Token::Star => write!(f, "*"),
+            Token::Slash => write!(f, "/"),
+            Token::IntLit(val) => write!(f, "int {}", val),
+            Token::Semi => write!(f, ";"),
+            Token::Print => write!(f, "print")
         }
     }
 }
 
-#[derive(Clone)]
-struct ASTNode {
-    op: ASTNodeOp,
-    left: Option<Box<ASTNode>>,
-    right: Option<Box<ASTNode>>
-}
-
-impl ASTNode {
-    fn new(op: ASTNodeOp, left: Box<ASTNode>, right: Box<ASTNode>) -> Self {
-        Self {
-            op: op,
-            left: Some(left),
-            right: Some(right)
-        }
-    }
-
-    fn mk_ast_node(op: ASTNodeOp) -> Self {
-        Self {
-            op: op,
-            left: None,
-            right: None
-        }
-    }
-
-    fn mk_ast_unary(op: ASTNodeOp, left: Box<ASTNode>) -> Self {
-        Self {
-            op: op,
-            left: Some(left),
-            right: None
-        }
-    }
+enum ASTNode {
+    Add(Box<ASTNode>, Box<ASTNode>),
+    Sub(Box<ASTNode>, Box<ASTNode>),
+    Mul(Box<ASTNode>, Box<ASTNode>),
+    Div(Box<ASTNode>, Box<ASTNode>),
+    IntLit(i64)
 }
 
 fn chrpos(s: &str, c: char) -> i64 {
@@ -79,9 +50,10 @@ struct Compiler {
     index: usize,
     line: i64,
     putback: char,
-    token: Token,
-    module_ctx: Context
+    token: Token
 }
+
+const TEXT_LEN_LIMIT: i64 = 512;
 
 impl<'a> Compiler {
     fn next(&mut self) -> char {
@@ -146,9 +118,18 @@ impl<'a> Compiler {
                 self.token = Token::Slash;
                 true
             },
+            ';' => {
+                self.token = Token::Semi;
+                true
+            },
             _ => {
                 if c.is_digit(10) {
                     self.token = Token::IntLit(self.scanint(c));
+                    true
+                } else if (c.is_alphabetic()) || (c == '_') {
+                    let text = self.scanident(c);
+                    let new_token = self.keyword(text.clone());
+                    self.token = new_token.expect(&format!("Unrecognized symbol {} on line {}", text, self.line));
                     true
                 } else {
                     panic!("Unrecognized character {} on line {}", c, self.line);
@@ -176,6 +157,35 @@ impl<'a> Compiler {
         val
     }
 
+    fn scanident(&mut self, c: char) -> String {
+        let mut i = 0;
+        let mut buf: String = String::new();
+        let mut ch: char = c;
+
+        while (ch.is_alphabetic()) || (ch.is_digit(10)) || (ch == '_') {
+            if i == (TEXT_LEN_LIMIT - 1) {
+                panic!("identifier too long on line {}", self.line);
+            } else if i < (TEXT_LEN_LIMIT - 1) {
+                buf += &String::from(ch);
+                i += 1;
+            }
+
+            ch = self.next();
+        }
+
+        self.putback = ch;
+        buf += "\0";
+        buf
+    }
+
+    fn keyword(&self, s: String) -> Option<Token> {
+        if s == "print\0" {
+            Some(Token::Print)
+        } else {
+            None
+        }
+    }
+
     fn new(filename: &str) -> Self {
         let mut f = File::open(filename).expect("no file found");
         let metadata = std::fs::metadata(filename).expect("unable to read metadata");
@@ -187,26 +197,13 @@ impl<'a> Compiler {
             putback: '\n',
             in_data: buffer.to_owned(),
             index: 0,
-            token: Token::Eof,
-            module_ctx: Context::create()
-        }
-    }
-
-    fn arithop(&self, token: Token) -> ASTNodeOp {
-        match token {
-            Token::Plus => ASTNodeOp::Add,
-            Token::Minus => ASTNodeOp::Subtract,
-            Token::Star => ASTNodeOp::Multiply,
-            Token::Slash => ASTNodeOp::Divide,
-            _ => {
-                panic!("Invalid token on line {}", self.line);
-            }
+            token: Token::Eof
         }
     }
 
     fn number(&mut self) -> ASTNode {
         if let Token::IntLit(val) = self.token {
-            let node = ASTNode::mk_ast_node(ASTNodeOp::IntLit(val));
+            let node = ASTNode::IntLit(val);
             self.scan();
             node
         } else {
@@ -222,17 +219,27 @@ impl<'a> Compiler {
         let mut left: ASTNode = self.mul_expr();
         let mut token: Token = self.token.clone();
 
-        if let Token::Eof = token.clone() {
+        if matches!(token, Token::Semi) {
             return left;
         }
 
         loop {
             self.scan();
             let right = self.mul_expr();
-            left = ASTNode::new(self.arithop(token), Box::new(left), Box::new(right));
+
+            left = match token {
+                Token::Plus => ASTNode::Add(Box::new(left), Box::new(right)),
+                Token::Minus => ASTNode::Sub(Box::new(left), Box::new(right)),
+                Token::Star => ASTNode::Mul(Box::new(left), Box::new(right)),
+                Token::Slash => ASTNode::Div(Box::new(left), Box::new(right)),
+                _ => {
+                    panic!("Invalid token on line {}", self.line);
+                }
+            };
+            
             token = self.token.clone();
 
-            if let Token::Eof = token.clone() {
+            if matches!(token, Token::Semi) {
                 break;
             }
         }
@@ -244,26 +251,33 @@ impl<'a> Compiler {
         let mut left: ASTNode = self.number();
         let mut token: Token = self.token.clone();
 
-        if let Token::Eof = token.clone() {
+        if matches!(token, Token::Semi) {
             return left;
         }
 
         while (matches!(token, Token::Star)) || (matches!(token, Token::Slash)) {
             match token.clone() {
-                Token::Eof => break,
-                Token::Plus => break,
-                Token::Minus => break,
                 Token::Star => (),
                 Token::Slash => (),
-                Token::IntLit(_) => break
+                _ => break
             }
 
             self.scan();
             let right = self.number();
-            left = ASTNode::new(self.arithop(token), Box::new(left), Box::new(right));
+
+            left = match token {
+                Token::Plus => ASTNode::Add(Box::new(left), Box::new(right)),
+                Token::Minus => ASTNode::Sub(Box::new(left), Box::new(right)),
+                Token::Star => ASTNode::Mul(Box::new(left), Box::new(right)),
+                Token::Slash => ASTNode::Div(Box::new(left), Box::new(right)),
+                _ => {
+                    panic!("Invalid token on line {}", self.line);
+                }
+            };
+
             token = self.token.clone();
 
-            if let Token::Eof = token.clone() {
+            if matches!(token, Token::Semi) {
                 break;
             }
         }
@@ -271,79 +285,66 @@ impl<'a> Compiler {
         left
     }
 
-    fn interpret_ast(&mut self, node: ASTNode) -> i64 {
-        let mut left_val: i64 = 0;
-        let mut right_val: i64 = 0;
-
-        if let Some(left_node) = node.left.clone() {
-            left_val = self.interpret_ast(*left_node);
-        }
-
-        if let Some(right_node) = node.right.clone() {
-            right_val = self.interpret_ast(*right_node);
-        }
-
-        if let ASTNodeOp::IntLit(val) = node.op.clone() {
-            println!("int {}", val);
+    fn match_token(&mut self, ttoken: Token, tstr: &str) {
+        if matches!(&self.token, ttoken) {
+            self.scan();
         } else {
-            println!("{} {} {}", left_val, node.op.clone(), right_val);
-        }
-
-        match node.op.clone() {
-            ASTNodeOp::Add => left_val + right_val,
-            ASTNodeOp::Subtract => left_val - right_val,
-            ASTNodeOp::Multiply => left_val * right_val,
-            ASTNodeOp::Divide => left_val / right_val,
-            ASTNodeOp::IntLit(val) => val
+            panic!("{} expected on line {}", tstr, self.line);
         }
     }
 
-    fn build_ast(&'a self, node: ASTNode, builder: &'a Builder) -> BasicValueEnum<'a> {
-        let mut left_val: Option<BasicValueEnum> = None;
-        let mut right_val: Option<BasicValueEnum> = None;
+    fn semi(&mut self) {
+        self.match_token(Token::Semi, ";");
+    }
 
-        if let Some(left_node) = node.left.clone() {
-            left_val = Some(self.build_ast(*left_node, builder));
+    fn statements(&'a mut self, builder: &'a Builder, printf_fn: FunctionValue<'a>, module_ctx: &'a Context) {
+        loop {
+            self.match_token(Token::Print, "print");
+            let tree = self.expr();
+            let ret_val = self.build_ast(tree, builder, module_ctx);
+            self.gen_print(builder, ret_val, printf_fn, module_ctx);
+            self.semi();
+
+            if matches!(self.token, Token::Eof) {
+                return;
+            }
         }
+    }
 
-        if let Some(right_node) = node.right.clone() {
-            right_val = Some(self.build_ast(*right_node, builder));
-        }
-
-        match node.op.clone() {
-            ASTNodeOp::Add => builder.build_int_add(left_val.unwrap().into_int_value(), right_val.unwrap().into_int_value(), "add").as_basic_value_enum(),
-            ASTNodeOp::Subtract =>  builder.build_int_sub(left_val.unwrap().into_int_value(), right_val.unwrap().into_int_value(), "sub").as_basic_value_enum(),
-            ASTNodeOp::Multiply => builder.build_int_mul(left_val.unwrap().into_int_value(), right_val.unwrap().into_int_value(), "mul").as_basic_value_enum(),
-            ASTNodeOp::Divide => builder.build_int_signed_div(left_val.unwrap().into_int_value(), right_val.unwrap().into_int_value(), "div").as_basic_value_enum(),
-            ASTNodeOp::IntLit(val) => {
-                let val_ptr = builder.build_alloca(self.module_ctx.i32_type(), "int");
-                builder.build_store(val_ptr, self.module_ctx.i32_type().const_int(val as u64, false));
+    fn build_ast(&'a self, node: ASTNode, builder: &'a Builder, module_ctx: &'a Context) -> BasicValueEnum<'a> {
+        match node {
+            ASTNode::Add(left, right) => builder.build_int_add(self.build_ast(*left, builder, module_ctx).into_int_value(), self.build_ast(*right, builder, module_ctx).into_int_value(), "add").as_basic_value_enum(),
+            ASTNode::Sub(left, right) =>  builder.build_int_sub(self.build_ast(*left, builder, module_ctx).into_int_value(), self.build_ast(*right, builder, module_ctx).into_int_value(), "sub").as_basic_value_enum(),
+            ASTNode::Mul(left, right) => builder.build_int_mul(self.build_ast(*left, builder, module_ctx).into_int_value(), self.build_ast(*right, builder, module_ctx).into_int_value(), "mul").as_basic_value_enum(),
+            ASTNode::Div(left, right) => builder.build_int_signed_div(self.build_ast(*left, builder, module_ctx).into_int_value(), self.build_ast(*right, builder, module_ctx).into_int_value(), "div").as_basic_value_enum(),
+            ASTNode::IntLit(val) => {
+                let val_ptr = builder.build_alloca(module_ctx.i32_type(), "int");
+                builder.build_store(val_ptr, module_ctx.i32_type().const_int(val as u64, false));
                 builder.build_load(val_ptr, "int_val")
             }
         }
     }
 
-    fn gen_print(&'a self, builder: &'a Builder, val: BasicValueEnum<'a>, print_fn: FunctionValue<'a>) {
-        let const_string = self.module_ctx.const_string(b"%d\n\0", true);
+    fn gen_print(&'a self, builder: &'a Builder, val: BasicValueEnum<'a>, print_fn: FunctionValue<'a>, module_ctx: &'a Context) {
+        let const_string = module_ctx.const_string(b"%d\n\0", true);
         let fmt_ptr = builder.build_alloca(const_string.get_type(), "fmt");
         builder.build_store(fmt_ptr, const_string);
-        let fmt_string_ptr = builder.build_bitcast(fmt_ptr, self.module_ctx.i8_type().ptr_type(AddressSpace::Generic), "fmt_ptr");
+        let fmt_string_ptr = builder.build_bitcast(fmt_ptr, module_ctx.i8_type().ptr_type(AddressSpace::Generic), "fmt_ptr");
         builder.build_call(print_fn, &[fmt_string_ptr.into(), val.into()], "print");
     }
 
-    fn generate_code(&self, node: ASTNode, out_file: &Path) {
-        let module = self.module_ctx.create_module("main_module");
-        let fn_type = self.module_ctx.i32_type().fn_type(&[self.module_ctx.i32_type().into()], false);
+    fn generate_code(&mut self, out_file: &Path) {
+        let module_ctx = Context::create();
+        let module = module_ctx.create_module("main_module");
+        let fn_type = module_ctx.i32_type().fn_type(&[module_ctx.i32_type().into()], false);
         let main_fn = module.add_function("main", fn_type, None);
-        let const_string = self.module_ctx.const_string(b"\0", true);
-        let printf_fn_type = self.module_ctx.i32_type().fn_type(&[self.module_ctx.i8_type().ptr_type(AddressSpace::Generic).into()], true);
+        let printf_fn_type = module_ctx.i32_type().fn_type(&[module_ctx.i8_type().ptr_type(AddressSpace::Generic).into()], true);
         let printf_fn = module.add_function("printf", printf_fn_type, Some(Linkage::External));
-        let builder = self.module_ctx.create_builder();
-        let entry = self.module_ctx.append_basic_block(main_fn, "entry");
+        let builder = module_ctx.create_builder();
+        let entry = module_ctx.append_basic_block(main_fn, "entry");
         builder.position_at_end(entry);
-        let ret_value = self.build_ast(node, &builder);
-        self.gen_print(&builder, ret_value, printf_fn);
-        let good_return = self.module_ctx.i32_type().const_int(0, false);
+        self.statements(&builder, printf_fn, &module_ctx);
+        let good_return = module_ctx.i32_type().const_int(0, false);
         builder.build_return(Some(&good_return));
         module.set_triple(&TargetMachine::get_default_triple());
         let res = module.verify();
@@ -363,11 +364,6 @@ impl<'a> Compiler {
             panic!("{}", errors);
         }
     }
-
-    fn run(&mut self) -> ASTNode {
-        self.scan();
-        self.expr()
-    }
 }
 
 fn usage(prog: String) {
@@ -383,8 +379,8 @@ fn main() {
     }
 
     let mut compiler = Compiler::new(&args[1].clone());
-    let node: ASTNode = compiler.run();
+    compiler.scan();
     let mut out_file = Path::new(".").join(Path::new(&args[1]).file_name().unwrap());
     out_file.set_extension("o");
-    compiler.generate_code(node, out_file.as_path());
+    compiler.generate_code(out_file.as_path());
 }
